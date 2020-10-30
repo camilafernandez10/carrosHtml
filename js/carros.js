758b2cad10ac51d11b71597ecea624e888930151
Miguel Rippe <miguelrippe01@gmail.com>
2020-10-29 19:40:29 -0500

    agregando carros

--- js/carros.js
+++ js/carros.js
@@ -39,7 +39,7 @@ function colocarCard(carro, idmar) {
         '<img src="' + carro.logoCarro + '">' +
         '</div>' +
         '<div class="content">' +
-        '<a class="header">' + carro.modelo + '</a>'+
+        '<a class="header"></a>' +carro.modelo +
         '<span class="meta">Precio: </span>' +
         '<span class="right floated">' +
         '<i class="heart outline like icon"></i>' +
