b3a971a284db5727100740859a5c4a1b0344ecb6
Miguel Rippe <miguelrippe01@gmail.com>
2020-11-30 11:50:11 -0500

    agregar cambios

---
+++ js/actions.js
@@ -0,0 +1,55 @@
+$("#btnMotor").click(function () {
+    mostrar_ocultar('motor');
+    $('#color').hide(); $('#cilindraje').hide(); $('#potencia').hide();
+    $('#fullequipo').hide(); $('#traccion').hide(); $('#frenos').hide(); $('#seguridad').hide();
+});
+$("#btnColor").click(function () {
+    mostrar_ocultar('color');
+    $('#motor').hide(); $('#cilindraje').hide(); $('#potencia').hide();
+    $('#fullequipo').hide(); $('#traccion').hide(); $('#frenos').hide(); $('#seguridad').hide();
+});
+$("#btnCilindraje").click(function () {
+    mostrar_ocultar('cilindraje');
+    $('#color').hide(); $('#motor').hide(); $('#potencia').hide();
+    $('#fullequipo').hide(); $('#traccion').hide(); $('#frenos').hide(); $('#seguridad').hide();
+});
+$("#btnPotencia").click(function () {
+    mostrar_ocultar('potencia');
+    $('#color').hide(); $('#cilindraje').hide(); $('#motor').hide();
+    $('#fullequipo').hide(); $('#traccion').hide(); $('#frenos').hide(); $('#seguridad').hide();
+});
+$("#btnFull").click(function () {
+    mostrar_ocultar('fullequipo');
+    $('#color').hide(); $('#cilindraje').hide(); $('#potencia').hide();
+    $('#motor').hide(); $('#traccion').hide(); $('#frenos').hide(); $('#seguridad').hide();
+});
+$("#btnTraccion").click(function () {
+    mostrar_ocultar('traccion');
+    $('#color').hide(); $('#cilindraje').hide(); $('#potencia').hide();
+    $('#fullequipo').hide(); $('#motor').hide(); $('#frenos').hide(); $('#seguridad').hide();
+});
+$("#btnFrenos").click(function () {
+    mostrar_ocultar('frenos');
+    $('#color').hide(); $('#cilindraje').hide(); $('#potencia').hide();
+    $('#fullequipo').hide(); $('#traccion').hide(); $('#motor').hide(); $('#seguridad').hide();
+});
+$("#btnSeguridad").click(function () {
+    mostrar_ocultar('seguridad');
+    $('#color').hide(); $('#cilindraje').hide(); $('#potencia').hide();
+    $('#fullequipo').hide(); $('#traccion').hide(); $('#frenos').hide(); $('#motor').hide();
+});
+
+
+
+function mostrar_ocultar(id) {
+    var elemento = document.getElementById(id);
+    if (!elemento) {
+        return true;
+    }
+    if (elemento.style.display == "none") {
+        elemento.style.display = "block"
+    } else {
+        elemento.style.display = "none"
+    };
+    return true;
+};
