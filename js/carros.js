f12b0ef56c49f436169d762fc6c51e78e8aabaa9
Miguel Rippe <miguelrippe01@gmail.com>
2020-11-30 12:33:57 -0500

    agregar cambios en front

--- js/carros.js
+++ js/carros.js
@@ -1,19 +1,19 @@
 var carros = [];
 
 traerCarros()
-function traerCarros(){
+function traerCarros() {
     $.ajax({
         method: 'GET',
         url: '/cgi-bin/carrosServidor/main.py',
-        dataType:"json",
-        success: function(rta){
-        console.log(rta[0].idMarca)
-        carros = rta;
-        console.log(carros)
-        cargarCarros();
+        dataType: "json",
+        success: function (rta) {
+            console.log(rta[0].idMarca)
+            carros = rta;
+            console.log(carros)
+            cargarCarros();
         },
-        error: function(rta){
-        console.log(JSON.stringify(rta))
+        error: function (rta) {
+            console.log(JSON.stringify(rta))
         }
     });
 
@@ -27,19 +27,19 @@ function cargarCarros() {
     for (var i = 0; i < carros.length; i++) {
         if (parseInt(carros[i].idMarca) === parseInt(idmar)) {
             colocarCard(carros[i], idmar);
-        } else if(isNaN(parseInt(idmar))){
+        } else if (isNaN(parseInt(idmar))) {
             colocarCard(carros[i], idmar);
         }
     }
 }
 
 function colocarCard(carro, idmar) {
-    var texto = '<div id='+ carro.id + ' class="ui link card product">' +
+    var texto = '<div id=' + carro.id + ' class="ui link card product">' +
         '<div class="image">' +
         '<img src="' + carro.logoCarro + '">' +
         '</div>' +
         '<div class="content">' +
-        '<a class="header">' + carro.modelo + '</a>'+
+        '<a class="header">' + carro.modelo + '</a>' +
         '<span class="meta">Precio: </span>' +
         '<span class="right floated">' +
         '<i class="heart outline like icon"></i>' +
@@ -120,14 +120,41 @@ var app = new Vue({
         this.f();
     },
 })
+
 $("#tarjetasCarros").on("click", ".link.card", function () {
     idcar = $(this).prop("id");
-    img = $(this).prop("logoCarro");
-    modelo = $(this).prop("modelo");
-    precio = $(this).prop("precio");
-    console.log(idcar)
-    console.log(img)
-    console.log(modelo)
-    console.log(precio)
-    //colocarCard2(idcar, img, modelo, precio);
+    for (var i = 0; i < carros.length; i++) {
+        if (parseInt(carros[i].id) === parseInt(idcar)) {
+            colocarCard2(carros[i]);
+        }
+    }
 });
+
+function colocarCard(carro) {
+    var texto = '<div id=' + carro.id + ' class="ui link card product">' +
+        '<div class="image">' +
+        '<img src="' + carro.logoCarro + '">' +
+        '</div>' +
+        '<div class="content">' +
+        '<a class="header">' + carro.modelo + '</a>' +
+        '<span class="meta">Precio: </span>' +
+        '<span class="right floated">' +
+        '<i class="heart outline like icon"></i>' +
+        '17 likes' +
+        '</span>' +
+        '<i class="dollar sign icon"></i>' +
+        carro.precio +
+        '</div>' +
+        '<div class="extra content">' +
+        '<div class="ui large transparent left icon input">' +
+        '</div>' +
+        '<div class="ui vertical animated button" tabindex="0">' +
+        '<div class="hidden content">Añadir al carrito</div>' +
+        '<div class="visible content">' +
+        '<i class="shop icon"></i>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+    $("#tarjetasCarros").append(texto);
+}
