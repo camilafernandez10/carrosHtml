b6eb7c88627e8ed2db9c48ea7cf9c3e213d3e96d
Miguel Rippe <miguelrippe01@gmail.com>
2020-11-30 18:05:53 -0500

    agregar cambios en front1

--- js/actions.js
+++ js/actions.js
@@ -60,6 +60,8 @@ $("#btnVolver2").click(function () {
     document.getElementById('content2').style.display = 'block';
     document.getElementById('content1').style.display = 'none';
     document.getElementById('content3').style.display = 'none';
+    $('#cedula').empty();
+    $('#tema').empty();
 });
 $("#btnReservar").click(function () {
     document.getElementById('content3').style.display = 'block';
